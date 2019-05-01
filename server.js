e799b802d40ee5c85796204e3cfaa36e4de4567d
RaduMirceaAndrei00 <mirad28@yahoo.com>
2019-05-01 23:24:13 +0300

    Am facut beckend-ul pentru inregistrarea utilizatorilor si am reusit sa le criptez email-ul pentru a-l putea trimitea ca si cod de validare in emailu-ul de validare

---
+++ server.js
@@ -0,0 +1,108 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+
+var cryptoJS = require('crypto-js');
+var AES =  require('crypto-js/aes');
+var SHA256 = require('crypto-js/sha256');
+
+var url = 'mongodb://localhost:27017/db';
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8084;
+
+var router = express.Router();
+
+var KEY = "5166546A576E5A72";
+
+router.use(function(req, res, next){
+    console.log('Something is happening');
+    next();
+});
+
+router.get('/', function(req, res){
+    res.json({ message: "Ura! Bun venit la api-ul nostru al aplicatiei BestNotes!" });
+});
+
+router.post('/users/register', function(req, res){
+    var item = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        userName: req.body.userName,
+        password: req.body.password,
+        passwordConfirmation: req.body.passwordConfirmation,
+        phoneNumber: req.body.phoneNumber,
+        active: false
+    };
+    if( item.password!==item.passwordConfirmation ){
+        res.json({ message: "Parola din campul parola difera de cea din campul confirmare parola, va rog sa le introduceti din nou" });
+        return;
+    }
+    if( item.firstName==="" ){
+        res.json({ message: "Nu ati completat campul prenume" });
+        return;
+    }
+    if( item.lastName==="" ){
+        res.json({ message: "Nu ati completat campul nume" });
+        return;
+    }
+    if( item.email==="" ){
+        res.json({ message: "Nu ati completat campul email" });
+        return;
+    }
+    if( item.userName==="" ){
+        res.json({ message: "Nu ati completat campul nume de utilizator" });
+        return;
+    }
+    if( item.password==="" ){
+        res.json({ message: "Nu ati completat campul parola" });
+        return;
+    }
+    if( item.passwordConfirmation==="" ){
+        res.json({ message: "Nu ati completat campul comfirmare parola" });
+        return;
+    }
+    if( item.phoneNumber==="" ){
+        res.json({ message: "Nu ati completat campul numar de telefon" });
+        return;
+    }
+
+    mongo.connect(url, { useNewUrlParser: true }, function(err, db){
+        if( err )
+            res.send(err);
+        var dbo = db.db("mydb");
+        Users=dbo.collection('user-data');
+        Users.findOne({ email:item.email }, function(err, user){
+            if(err)
+                res.send(err);
+            if(user){
+                res.json({ message: "This email adress is already used" });
+                return;
+            }
+            Users.insertOne(item, function(err, result){
+                if(err)
+                    res.send(err);
+                res.json({ message: "Utilizator creat cu succes!" }); 
+
+                //!!! AICI CRIPTEZ email-ul pentru a crea confirmation token-ul
+                var activationToken = cryptoJS.AES.encrypt(item.email, KEY).toString();
+                console.log(activationToken);
+                activationToken = encodeURIComponent(activationToken);
+                console.log(activationToken);
+                activationToken = decodeURIComponent(activationToken);
+                var decryptedToken = cryptoJS.AES.decrypt(activationToken, KEY);
+                console.log(decryptedToken.toString(cryptoJS.enc.Utf8));
+                db.close();
+            });
+        });
+    });
+});
+
+app.use('/',router);
+
+app.listen(port);
+console.log('Magin happens on port ' + port);
